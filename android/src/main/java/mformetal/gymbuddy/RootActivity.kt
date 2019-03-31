04d65e3832c3ef8d72fe7be8c86ddbc4176a6aed
Miles Peele <milesbpeele@gmail.com>
2019-03-31 17:05:45 -0400

    Gonna use single activity, no need for base class

--- android/src/main/java/mformetal/gymbuddy/RootActivity.kt
+++ android/src/main/java/mformetal/gymbuddy/RootActivity.kt
@@ -1,12 +1,9 @@
 package mformetal.gymbuddy
 
 import android.os.Bundle
-import androidx.core.app.ComponentActivity
 import androidx.fragment.app.FragmentActivity
-import mformetal.gymbuddy.base.android.DelegateActivity
 import mformetal.gymbuddy.extensions.takeIfNull
 import mformetal.gymbuddy.home.*
-import mformetal.gymbuddy.kedux.Dispatcher
 
 class RootActivity : FragmentActivity() {
 
