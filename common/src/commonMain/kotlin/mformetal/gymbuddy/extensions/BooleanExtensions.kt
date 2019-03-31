e8016476fbd66ef81549651b447195b74d2de1ef
Miles Peele <milesbpeele@gmail.com>
2019-03-31 14:09:48 -0400

    Don't include android ish in common

--- common/src/commonMain/kotlin/mformetal/gymbuddy/extensions/BooleanExtensions.kt
+++ common/src/commonMain/kotlin/mformetal/gymbuddy/extensions/BooleanExtensions.kt
@@ -1,3 +1,3 @@
 package mformetal.gymbuddy.extensions
 
-fun <T : Any> Boolean.takeIfFalse(doIfTrue: () -> T) : T? = if (!this) doIfTrue() else null
+fun <T : Any> Boolean.takeIfFalse(doIfTrue: () -> T) : T? = if (not()) doIfTrue() else null
