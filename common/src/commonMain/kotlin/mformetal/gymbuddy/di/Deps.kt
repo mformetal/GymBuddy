4197f1c46ba9debcb00ee79a3c52f2339c372055
Miles Peele <milesbpeele@gmail.com>
2019-04-07 09:52:02 -0400

    Create root module

--- common/src/commonMain/kotlin/mformetal/gymbuddy/di/Deps.kt
+++ common/src/commonMain/kotlin/mformetal/gymbuddy/di/Deps.kt
@@ -1,4 +1,23 @@
 package mformetal.gymbuddy.di
 
-class Deps {
+import com.soywiz.klock.Month
+import mformetal.gymbuddy.kedux.arch.Store
+import mformetal.gymbuddy.kedux.state.AppState
+import mformetal.gymbuddy.kedux.state.CalendarState
+import org.kodein.di.Kodein
+import org.kodein.di.erased.bind
+import org.kodein.di.erased.singleton
+
+object Deps {
+
+    val kodein = Kodein.invoke {
+        bind() from singleton {
+            val appState = AppState(
+                    calendarState = CalendarState(
+                            Month.April
+                    )
+            )
+            Store(appState)
+        }
+    }
 }
